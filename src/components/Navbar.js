885083909836e31cbcc1e56f166eedeb49decc88
Satyam kumar <144627386+satyamkumar46@users.noreply.github.com>
2025-08-10 21:10:07 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        
+        <div>
+
+            <nav className="bg-bgDark py-4">
+                <h1 className='text-white text-center text-3xl font-bold'>Top Courses</h1>
+            </nav>
+
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/Spinner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Spinner() {
+    return (
+        
+        <div>
+
+            <div className='spinner'></div>
+            <p>Loading...</p>
+
+        </div>
+    )
+}
+
+export default Spinner
